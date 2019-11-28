7a61e0cc1f0fe6bc769350bfddd51cb1ec9e86b7
tengzx <tzxit@sina.com>
2019-11-28 19:40:37 +0800

    整合swagger

--- springbootlearn/src/main/java/com/tzx/springbootlearn/controller/UserController.java
+++ springbootlearn/src/main/java/com/tzx/springbootlearn/controller/UserController.java
@@ -2,13 +2,18 @@ package com.tzx.springbootlearn.controller;
 
 import com.tzx.springbootlearn.model.User;
 import com.tzx.springbootlearn.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@Api(tags = "用户设置")
 @RequestMapping("/user")
 public class UserController {
 
@@ -16,6 +21,8 @@ public class UserController {
   private UserService userService;
 
   @GetMapping("/{id}")
+  @ApiOperation("根据id查询用户")
+  @ApiImplicitParam(name = "id", value = "用户id", defaultValue = "1", required = true)
   public User GetUser(@PathVariable int id){
     return userService.findById(id);
   }
--- springbootlearn/src/main/java/com/tzx/springbootlearn/model/User.java
+++ springbootlearn/src/main/java/com/tzx/springbootlearn/model/User.java
@@ -1,12 +1,19 @@
 package com.tzx.springbootlearn.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel(value = "用户")
 public class User {
+  @ApiModelProperty("主键")
   private Integer id;
+  @ApiModelProperty("用户名")
   private String userName;
+  @ApiModelProperty("用户密码")
   private String passWord;
+  @ApiModelProperty("用户真实名字")
   private String realName;
 
   @Override
