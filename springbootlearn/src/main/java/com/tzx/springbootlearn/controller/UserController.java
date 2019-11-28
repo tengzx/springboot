1ae06fa45d9759cf9b7e5bbfe108462c5122d892
tengzx <tzxit@sina.com>
2019-11-28 18:04:12 +0800

    整合mybatis

--- springbootlearn/src/main/java/com/tzx/springbootlearn/controller/UserController.java
+++ springbootlearn/src/main/java/com/tzx/springbootlearn/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.tzx.springbootlearn.controller;
 
+import com.tzx.springbootlearn.model.User;
 import com.tzx.springbootlearn.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,8 @@ public class UserController {
   @Autowired
   private UserService userService;
 
-  @RequestMapping("/{id}")
-  public String GetUser(@PathVariable int id){
-    return userService.findById(id).toString();
+  @GetMapping("/{id}")
+  public User GetUser(@PathVariable int id){
+    return userService.findById(id);
   }
 }
