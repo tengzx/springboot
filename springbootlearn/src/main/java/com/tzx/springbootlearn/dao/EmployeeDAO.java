1de0a935e173321d752312996e7a1111f5b43e9a
tengzx <tzxit@sina.com>
2019-11-28 16:14:07 +0800

    test

---
+++ springbootlearn/src/main/java/com/tzx/springbootlearn/dao/EmployeeDAO.java
@@ -0,0 +1,24 @@
+package com.tzx.springbootlearn.dao;
+
+import com.tzx.springbootlearn.model.Employee;
+import com.tzx.springbootlearn.model.Employees;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeDAO {
+  private static Employees list = new Employees();
+
+  static {
+    list.getEmployeeList().add(new Employee(1, "Lokesh", "Gupta", "howtodoinjava@gmail.com"));
+    list.getEmployeeList().add(new Employee(2, "Alex", "Kolenchiskey", "abc@gmail.com"));
+    list.getEmployeeList().add(new Employee(3, "David", "Kameron", "titanic@gmail.com"));
+  }
+
+  public Employees getAllEmployees() {
+    return list;
+  }
+
+  public void addEmployee(Employee employee) {
+    list.getEmployeeList().add(employee);
+  }
+}
